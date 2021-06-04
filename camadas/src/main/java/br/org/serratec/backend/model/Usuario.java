0f449580f34eca0b0ed1881b879dfa7f1551bd87
LuizCarlosRafael <82171431+LuizCarlosRafael@users.noreply.github.com>
2021-06-04 15:26:31 -0300

    Add files via upload
    Projeto Api Camadas

---
+++ camadas/src/main/java/br/org/serratec/backend/model/Usuario.java
@@ -0,0 +1,98 @@
+package br.org.serratec.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	private Long Id;
+	private String nome;
+	private String email;
+	private String perfil;
+	private String senha;
+	
+	public Usuario() {
+		
+	}
+			
+	public Usuario(Long id, String nome, String email, String perfil, String senha) {
+		super();
+		Id = id;
+		this.nome = nome;
+		this.email = email;
+		this.perfil = perfil;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+}
